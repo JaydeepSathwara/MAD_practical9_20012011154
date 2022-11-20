0268213cdd8580283a32d1a968bfce9a2eab5a93
jaydeep <jaydeepsathwara272@gmail.com>
2022-11-20 22:04:01 +0530

    Initial commit

---
+++ app/src/main/java/com/jaydeepsathwara/practical_9_154/SMSView.kt
@@ -0,0 +1,8 @@
+package com.jaydeepsathwara.practical_9_154
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SMSView(val phonenumber : String, val message : String) {
+
+}
---
+++ app/src/main/java/com/jaydeepsathwara/practical_9_154/sms_item_view.kt
@@ -0,0 +1,11 @@
+package com.jaydeepsathwara.practical_9_154
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sms_item_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_item_view)
+    }
+}
